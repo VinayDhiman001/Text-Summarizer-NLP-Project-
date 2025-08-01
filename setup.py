3a064ac7dcf38166ff25536d2aa6c4b6315380f3
VinayDhiman001 <vinusaha007@gmail.com>
2025-08-01 13:09:39 +0530

    requirements & installation done

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ __version__="0.0.0"
 
 REPO_NAME ="Text-Summarizer-NLP-Project-"
 AUTHOR_USER_NAME = "VinayDhiman001"
-SRC_REPO = "Text-Summarizer"
+SRC_REPO = "textSummarizer"
 AUTHOR_EMAIL = "vinusaha007@gmail.com"
 
 
